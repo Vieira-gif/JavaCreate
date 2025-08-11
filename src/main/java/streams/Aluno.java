9ab2fe26d03c24b676bde91dc7ecd083a188e1f0
João Vieira <mutantinotrue@outlook.com>
2025-08-10 22:51:25 -0300

    XamNim

--- src/main/java/streams/Aluno.java
+++ src/main/java/streams/Aluno.java
@@ -1,5 +1,7 @@
 package streams;
 
+import java.util.Objects;
+
 public class Aluno {
     final String nome;
     final double nota;
@@ -7,5 +9,17 @@ public class Aluno {
     Aluno(String nome, double nota) {
         this.nome = nome;
         this.nota = nota;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Aluno aluno)) return false;
+        return Double.compare(nota, aluno.nota) == 0 && Objects.equals(nome, aluno.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, nota);
     }
 }
