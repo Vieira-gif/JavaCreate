2c617a4d4b4bca43fda32c74b4e574057f4bdef6
João Vieira <mutantinotrue@outlook.com>
2025-10-28 18:45:25 -0300

    JPA - Classe para Excluir Pessoa

--- src/main/java/jdbc/FabricaConexao.java
+++ src/main/java/jdbc/FabricaConexao.java
@@ -1,19 +1,29 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
     public static Connection getConexao(){
-        final String url = "jdbc:mysql://localhost:3306/curso_java";
-        final String user = "root";
-        final String password = "root";
-
         try {
+            Properties prop = getProperties();
+            final String url = prop.getProperty("banco.url");
+            final String user =  prop.getProperty("banco.usuario");
+            final String password =  prop.getProperty("banco.senha");
+
             return DriverManager.getConnection(url, user, password);
-        } catch (SQLException e)  {
+        } catch (SQLException | IOException e)  {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Properties getProperties() throws IOException {
+        Properties prop = new Properties();
+        String path = "src/main/java/db.properties";
+        prop.load(FabricaConexao.class.getResourceAsStream(path));
+        return prop;
     }
 }
